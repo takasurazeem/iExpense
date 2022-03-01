49a3aa717b25993da20767e7db45e6426c09dc6a
Takasur Azeem <takasurazeem@gmail.com>
2022-03-01 08:36:51 +0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Takasur Azeem on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
