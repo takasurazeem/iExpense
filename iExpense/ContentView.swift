9eed77fa1f7ee38ec07a34faaad3a2969725cfe3
Takasur Azeem <takasurazeem@gmail.com>
2022-03-05 23:24:44 +0500

    iExpense app dev init
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Takasur Azeem on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
